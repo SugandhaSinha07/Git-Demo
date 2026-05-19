8d635de424356b3596a6f4e5aaf8da3b34a503ce
sugandha <sugandhasinhadiv@gmail.com>
2026-05-19 12:57:51 +0530

    Asian team commit

--- src/test/java/ECommerceProject/data/RestAPI.java
+++ src/test/java/ECommerceProject/data/RestAPI.java
@@ -1,9 +1,15 @@
 package ECommerceProject.data;
 
+import org.testng.annotations.Test;
+
 public class RestAPI {
-	
+	@Test
 	public void practice() {
 		System.out.println("Ok");
+		//Added for branch commit
+		System.out.println("hk");
+		System.out.println("hk");
+		
 	}
 
 }
